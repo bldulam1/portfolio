0621bcfb582b5fc13cf9ce4adab8c79fb46d0447
Brendon Dulam <brendon.dulam@veoneer.com>
2019-05-25 11:28:49 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/bldulam1/portfolio/precache-manifest.f32ac80e33f83e453dadfbb338da1870.js"
+  "/portfolio/precache-manifest.c05693a7b6312a5bb096fe28e8ad9d31.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/bldulam1/portfolio/index.html", {
+workbox.routing.registerNavigationRoute("/portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
